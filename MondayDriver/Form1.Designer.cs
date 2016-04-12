fcafd37b6854d815a102a365c214b7683c72e2ad
lindi-imre <lindi.imre@gmail.com>
2016-04-12 15:22:20 +0200

    Initial commit

---
+++ MondayDriver/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MondayDriver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(113, 12);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(353, 21);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drives:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(503, 186);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(13, 281);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(94, 281);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 316);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox);
+            this.Name = "Form1";
+            this.Text = "MondayDriver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ MondayDriver/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MondayDriver
+{
+    public partial class Form1 : Form
+    {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+        private FileInfo[] files;
+        private DirectoryInfo[] directories;
+
+        static int listBoxSelectedItem;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            listBox1.Enabled = false;
+
+            foreach (DriveInfo drive in allDrives)
+            {
+                comboBox.Items.Add(drive.Name + " (" + drive.DriveType + ")");
+            }
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBox.SelectedIndex;
+            textBox1.Text = allDrives[index].Name;
+
+            listBox1.Items.Clear();
+
+            if (index != null)
+            {
+                listBox1.Enabled = true;
+            }
+
+            DirectoryInfo dinfo = new DirectoryInfo(allDrives[index].Name);
+
+            files = dinfo.GetFiles();
+            directories = dinfo.GetDirectories();
+
+            Array.Sort(files, delegate (FileInfo f1, FileInfo f2) {
+                return f1.Name.CompareTo(f2.Name);
+            });
+
+            Array.Sort(directories, delegate (DirectoryInfo f1, DirectoryInfo f2) {
+                return f1.Name.CompareTo(f2.Name);
+            });
+
+            foreach (DirectoryInfo directory in directories)
+            {
+                listBox1.Items.Add(directory.Name);
+            }
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add(file.Name);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxSelectedItem = listBox1.SelectedIndex;
+            if ((listBoxSelectedItem + 1) > directories.Length)
+            {
+                textBox1.Text = files[listBoxSelectedItem - directories.Length].FullName;
+            }
+            else
+            {
+                textBox1.Text = directories[listBoxSelectedItem].FullName;
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
